98649db26f4d55084df528013c935b3fa6d14ca8
Shahmeer <shahmeerrar@gmail.com>
2025-06-04 16:34:49 +0500

    ctrl+backspace remove whole word behind the cursor

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,3 @@
-vim.keymap.set("i", "<C-BS>", "<C-W>", { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-BS>', '<C-W>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-BS>', 'db', { noremap = true, silent = true })
 
